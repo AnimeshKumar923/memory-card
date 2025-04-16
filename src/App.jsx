3b206c2f594187b729e739de21531e2bd144b8f6
Animesh Kumar <kumaranimesh923@gmail.com>
2025-04-16 17:09:05 +0530

    fix: improve grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,26 @@ import "./App.css";
 import Card from "./components/Card";
 
 function App() {
+  const images = [
+    { fileName: "jeff.jpg", altText: "Jeff image" },
+    { fileName: "sue.jpg", altText: "Emma Stone image" },
+    { fileName: "adam.jpg", altText: "Adam Warlock image" },
+    { fileName: "hela.jpg", altText: "Hela image" },
+    { fileName: "ironman.jpg", altText: "Iron Man image" },
+    { fileName: "luna.jpg", altText: "Luna Snow image" },
+    { fileName: "strange.jpg", altText: "Dr. Strange image" },
+    { fileName: "emma.jpg", altText: "Sue Storm image" },
+  ];
   return (
     <>
-      <div className="main">
-        <Card src="../assets/emma.jpg" alt={"emma stone image"}/>
+      <div className="grid-layout">
+        {images.map((image, index) => (
+          <Card
+            key={index}
+            src={`../assets/${image.fileName}`}
+            alt={image.altText}
+          />
+        ))}
       </div>
     </>
   );
